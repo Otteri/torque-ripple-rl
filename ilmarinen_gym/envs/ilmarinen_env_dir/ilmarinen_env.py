cddace6f496d5e241c8356c244f2a1f513f007ad
Joni Airaksinen <Otteri@users.noreply.github.com>
2020-08-02 00:26:58 +0300

    Torque based learning (#1)
    The agent learns to compensate for torque pulsations when action values are appropriate.

--- ilmarinen_gym/envs/ilmarinen_env_dir/ilmarinen_env.py
+++ ilmarinen_gym/envs/ilmarinen_env_dir/ilmarinen_env.py
@@ -6,88 +6,135 @@ import gym
 class IlmarinenEnv(gym.Env):
     sim = None
     step_num = 0
-    positive_reward_combo = 0
-    signal_prev = 0
+    angle_prev = 1.0
+    prev_actual = 0.0
+
+    last_compensation_value = 0
+    # Step size has to be enough small to be able to utilize this function,
+    # otherwise the finish signal can come too late.
+    def hasPeriodFinished(self):
+        MAX_ANGLE = 1.0
+        current_angle = self.sim.signal.getRotorElectricalAngleCtrl()
+        expected_change = current_angle - self.angle_prev
+        self.angle_prev = current_angle
+
+        # It is important to return the True a fraction-step before the rotation
+        # Because first change calculation is incorrect after rotation without
+        # special handling. (Angle is nonlinear function).
+        if (current_angle + expected_change) > MAX_ANGLE or \
+            self.step_num >= self.max_steps-1: # don't allow buffer overflow
+            return True
+        return False
+
+
+    # Zeroes signals
+    def clearPlot(self):
+        self.compensation_torque = np.zeros(self.max_steps+1) # compensation
+        self.actual_torque = np.zeros(self.max_steps+1) # cogging
+        self.rotor_angle = np.zeros(self.max_steps+1)
+        self.speed = np.zeros(self.max_steps+1)
 
     def __init__(self, visual=False, compensation=True):
         self.sim = Ilmarinen.SandboxApi()
         self.compensation = compensation
-        self.step_size = 0.0001
-        self.max_steps = 5000 # 0.001 * 1000 = 1 (s)
+        self.step_size = 0.001
+        self.max_steps = 251
+        self.rpm = 60
 
-        # self.rpm = 
-        # revolution_in_steps = (rpm / 60s) / step_size
-        self.revolution_in_steps = (60 / 60) / self.step_size
+        # Set Sim into learning mode:
+        self.sim.command.toggleQlr(True)
+        self.sim.command.toggleLearning(True)
+        self.revolution_in_steps = (self.rpm / 60) / self.step_size
+
+        # Run to stable and then beginning of a electrical period
+        self.sim.command.step(30)
+        while not self.hasPeriodFinished():
+            self.sim.command.step(0.001)
 
         # For rendering:
         if visual:
-            self.compensation_torque = np.zeros(self.max_steps+1) # compensation
-            self.actual_torque = np.zeros(self.max_steps+1) # cogging
+            self.clearPlot() # use to allocate memory
             self.time = np.arange(0, (self.max_steps+1) * self.step_size, self.step_size)
             plt.ion()
             self.figure = plt.figure()
             self.axle = self.figure.add_subplot(111)
             self.line1, = self.axle.plot(self.time, self.compensation_torque, 
-                color='darkorange', label='Compensation', linestyle='-', linewidth=0.5)
+                color='yellowgreen', label='Compensation torque', linestyle='-', linewidth=0.5)
             self.line2, = self.axle.plot(self.time, self.actual_torque, 
-                color='red', label="Actual", linestyle='-', linewidth=0.5)
-            plt.ylabel("Amplitude (p.u.)")
-            plt.xlabel("Time (s)")
-            plt.title("Torque plot")
-            plt.legend()
+                color='red', label="Actual torque", linestyle='-', linewidth=0.5)
+            plt.ylabel("Amplitude [pu.]")
+            plt.xlabel("Time [s]")
+            plt.legend(loc='upper right')
             axes = plt.gca()
-            axes.set_ylim([-1.0, 1.0])
+            axes.set_ylim([-0.70, 0.70])
 
-    def step(self, value):     
-        # The aim is to compensate disturbances, so we want the signal to
-        # stay steady. Give positive reward if the value stays ~constant.
-        # If the difference is huge, then give negative reward.
-
+    def step(self, value):
+        # The aim is to compensate disturbances,
+        # so we want the signal to stay steady:
         def getReward(reference, actual):
+            self.prev_actual = actual
             difference = abs(reference - actual)
-            reward = -difference # Use negative cost as reward:
-            return reward
+            cost = difference
+            return -cost
 
         # Done if some of the following conditions have occured:
         # 1) Max step number has been reached
         # 2) Fault has occured. Cannot continue
         # 3) Problem is solved if one full revolution with positive reward only
         def getDone():
-            done = self.step_num+1 >= self.max_steps \
-                   or self.sim.signal.getTripCode() != -1 #\
-                   #or self.positive_reward_combo >= self.revolution_in_steps
+            done = self.hasPeriodFinished()
             return done
 
         if self.compensation:
-            self.sim.signal.setCompensationTorque(float(value))
-        self.sim.command.step(0.01)
-        response_signal = self.sim.signal.gt_FA2TC_data_().n_meas
-        reward = getReward(self.sim.signal.getSpeedReference(), response_signal)
+            self.sim.signal.setAction(value)
+            self.last_compensation_value = value
+
+        self.sim.command.step(self.step_size)
+        reward = getReward(0, self.sim.signal.getSimActualTorque())
         done = getDone()
         info = self.sim.signal.getTripCode()
         torque = self.sim.signal.getSimActualTorqueFiltered()
-        state = np.array([torque, self.sim.signal.getRotorElectricalAngle()])
+        state = np.array([torque, self.sim.signal.getRotorElectricalAngleCtrl(), 0])
         self.step_num += 1
 
         return state, reward, done, info
 
+    # Fast training: do not actually reset the whole sim
+    # New period has started. Just reset steps.
     def reset(self):
-        del self.sim
         self.step_num = 0
-        self.last_compensation_value = 0
-        self.sim = Ilmarinen.SandboxApi()
-        randomization = np.random.uniform(0.0, 1.0)
-        self.sim.command.step(10 + randomization) # run past startup
         signal = self.sim.signal.getSimActualTorqueFiltered()
-        angle = self.sim.signal.getRotorElectricalAngle()
-        return np.array([signal, angle])
+        angle = self.sim.signal.getRotorElectricalAngleCtrl()
+        self.last_compensation_value = 0
+        return np.array([signal, angle, 0])
+
+        # HardReset -- use when trips. Restarts whole simulator
+    def hardReset(self):
+        del self.sim
+        self.clearPlot()
+        self.sim = Ilmarinen.SandboxApi()
+
+        # Run to stable and then beginning of a electrical period
+        self.sim.command.step(30)
+        while not self.hasPeriodFinished():
+            self.sim.command.step(0.001)
+
+        return self.reset()
 
     def render(self):
-        self.compensation_torque[self.step_num] = self.sim.signal.getCompensationTorque()
+        self.compensation_torque[self.step_num] = self.sim.signal.getAction()
+        self.actual_torque[self.step_num] = self.sim.signal.getSimActualTorque()
+        
         self.line1.set_ydata(self.compensation_torque)
-
-        self.actual_torque[self.step_num] = self.sim.signal.getSimActualTorqueFiltered()
         self.line2.set_ydata(self.actual_torque)
 
         self.figure.canvas.draw()
         self.figure.canvas.flush_events()
+
+# Good to know:
+# The period length is not fixed, because rotor rotation speed is not constant, due to
+# disturbances and applied compensation torque, which creates variaton to rotation speed. 
+# Therefore, it is possible to overrun constant size buffer. For this reason hasPeriodFinished
+# function checks that buffers are not getting overrun, and in case of exceeding buffers, a new 
+# period is started. This must be accounted when plotting the results. Some periods can be just
+# a few steps long and these should be ignored. Todo: get rid of this nuisance.
--- pulsar.py
+++ pulsar.py
@@ -8,13 +8,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Hyperparameters
-gamma = 0.95
-alpha = 0.10
-e = 50 # epsilon converges
+gamma = 0.99
+alpha = 0.6
+e = 500 # epsilon converges
 
 # Grid parameters
-angle_dt = 100 #1334 # Number of possible states with 2.5us step and 60rpm.
-actions = np.linspace(-0.10, 0.10, 5)
+angle_dt = 100 # Number of possible states
+actions = np.linspace(-0.23, 0.23, 7)
 th_min, th_max = 0, 1
 th_grid = np.linspace(th_min, th_max, angle_dt)
 
@@ -27,28 +27,18 @@ def createGrid():
 def loadGrid(location):
     print("Loading model from '{}'".format(location))  
     qtable = np.load(location)
-    print("Grid signature:", getGridSignature(qtable))
     return qtable
 
 # Indexing starts from 0
 def getClosestIdx(list, value):
     return (np.abs(list - value)).argmin()
 
-# Calculate 'signature' by summing all weights. Useful for checking
-# if load succeed. Simple but effective way, which is also easy to
-# implement on C++ side. Use only int due to floating point deviation.  
-def getGridSignature(grid):
-    total = 0.0
-    for value in np.nditer(grid): 
-        total += value
-    return int(total)
-
 # Parse script arguments
 def parseArgs(args=sys.argv[1:]):
     parser = argparse.ArgumentParser()
     parser.add_argument("--test", "-t", type=str, default=None, help="Model to be tested")
     parser.add_argument("--episodes", "-ep", type=int, default=500, help="Number of episodes to train for")
-    parser.add_argument("--render", action='store_true', help="Render compensation waveform")
+    parser.add_argument("--render", action='store_true', default=None, help="Render compensation waveform")
     parser.add_argument("--load", type=str, default=None, help="Load model and continue training")
     parser.add_argument("--no_compensation", action='store_true', default=None, help="Does not use or train compensator")
     return parser.parse_args(args)
@@ -57,10 +47,15 @@ def parseArgs(args=sys.argv[1:]):
 def plotReward(reward_history, average_reward_history):
     plt.close('all')
     episodes = np.arange(len(reward_history))
-    fig = plt.figure()
-    plt.plot(episodes, reward_history, color='lightblue', alpha=0.8)
-    plt.plot(episodes, average_reward_history, linewidth=0.8, color='b')
-    fig.savefig("reward-history")
+    fig, _ = plt.subplots(figsize=(16,10), dpi=100)
+    plt.plot(episodes, reward_history, label='Reward, episode total', color='lightblue', alpha=0.8)
+    plt.plot(episodes, average_reward_history, label='Reward, running average' ,linewidth=0.8, color='b')
+    plt.xlabel('Episode number', fontsize=14)
+    plt.ylabel('Reward', fontsize=14,)
+    plt.legend(loc='lower right')
+    plt.rc('grid', linestyle='dotted', color='silver')
+    plt.grid(True)
+    fig.savefig("reward-history.svg")
 
 # Returns best action if random number [0, 1] is larger than epsilon.
 # Hence, to always get best action, give epsilon < 0.
@@ -92,10 +87,11 @@ def test(env, qtable, episodes, render=False):
         print("Episode {}, total reward {:.2f}, trip {}, torque avg. {}".format(episode_number, reward_sum, info, 0))
 
 def train(env, qtable, episodes, render=False):
+    episode_number = 0
     reward_history = []
     average_reward_history = []
 
-    for episode_number in range(1, episodes+1):
+    while episode_number < episodes:
         reward_sum, timesteps = 0, 0
         done = False
         state = env.reset()
@@ -118,19 +114,24 @@ def train(env, qtable, episodes, render=False):
             timesteps += 1
 
             # Collect data for rendering
-            if render and episode_number % 1 == 0:
+            if render and episode_number % 1  == 0:
                 env.render()
 
-        print("Episode {}, total reward {:.2f}, trip {}, timesteps {}, torque avg. {}".format(episode_number, reward_sum, info, timesteps, 0))
+        # Unbeliavable result, do not use it. Currently, sum can be > -5, when episode is
+        # too short because of early episode termination due to exceeding buffers.
+        if reward_sum > -5.0:
+            continue
 
-        reward_history.append(reward_sum)   
-        avg = np.mean(reward_history[-10:]) if episode_number > 5 else np.mean(reward_history)
+        episode_number = episode_number + 1
+        reward_history.append(reward_sum)
+        avg = np.mean(reward_history[-100:]) if episode_number > 100 else np.mean(reward_history)
         average_reward_history.append(avg)
 
-        if episode_number % 50 == 0:
-            # TODO: save epsilon and rewards
+        if episode_number % 10 == 0 and episode_number > 50:
+            print("Episode {}, total reward {:.2f}, trip {}, timesteps {}".format(episode_number, reward_sum, info, timesteps))
+
+        if episode_number % 100 == 0:
             np.save("qtable.npy", qtable)
-            print("Grid signature:", getGridSignature(qtable))
             print("Epsilon:", epsilon)
             if not render: # cannot plot & render simultaneously
                 plotReward(reward_history, average_reward_history)
@@ -145,7 +146,7 @@ def main(args):
     else:
         env = gym.make('IlmarinenEnv2-v0')
 
-    # Load or create a new Qtable?
+    # Load or create a new Qtable
     if args.test is None:
         qtable = loadGrid(args.load) if args.load is not None else createGrid()
         train(env, qtable, args.episodes, args.render)
